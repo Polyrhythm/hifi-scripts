d3e0e2b6f5bfe9a07ee1883eb717e7e5bbbd9d40
Polyrhythm <ryan.joshua.jones@gmail.com>
2016-11-26 09:30:52 -0800

    Update test_frag.fs

--- test_frag.fs
+++ test_frag.fs
@@ -1,11 +1,13 @@
-vec4 getProceduralColor() {
-    vec2 uv = _position.xz;
+float getProceduralColors(inout vec3 diffuse, inout vec3 specular, inout float shininess) {
+    vec2 uv = _position.xz / iResolution.xy;
     vec2 p = -1.0 + 2.0 * uv;
 
     vec3 colour = vec3(1.0);
 
     float map = step(0.1, distance(p, vec2(0.0)));
 
-    return vec4(vec3(map, 1.0, 1.0), 1.0);
+    diffuse = vec4(vec3(map, 1.0, 1.0), 1.0);
+    
+    return 0.0;
 }
 
