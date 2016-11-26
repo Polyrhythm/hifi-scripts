b38ddba77b08b0624f8a34cbdc130aef0475e015
Polyrhythm <ryan.joshua.jones@gmail.com>
2016-11-26 09:37:33 -0800

    Update test_frag_entity.js

--- test_frag_entity.js
+++ test_frag_entity.js
@@ -2,11 +2,11 @@
 TestShader = function() {};
 TestShader.prototype.USER_DATA = {
     ProceduralEntity: {
-    shaderUrl: "https://raw.githubusercontent.com/Polyrhythm/hifi-scripts/master/test_frag.fs",
-    version: 2,
-    uniforms: {},
+        shaderUrl: "https://raw.githubusercontent.com/Polyrhythm/hifi-scripts/master/test_frag.fs",
+        version: 2,
+        uniforms: {},
     }
 }
 
-return new TestShader();
+return TestShader();
 
