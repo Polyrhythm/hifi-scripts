e3192d31a6f0aa5f38f044f706ffe671d75a4955
Polyrhythm <ryan.joshua.jones@gmail.com>
2016-11-26 09:08:31 -0800

    Create test_frag_entity.js

---
+++ test_frag_entity.js
@@ -0,0 +1,9 @@
+// https://raw.githubusercontent.com/Polyrhythm/hifi-scripts/master/test_frag.fs
+
+{
+    "ProceduralEntity": {
+        "shaderUrl": "https://raw.githubusercontent.com/Polyrhythm/hifi-scripts/master/test_frag.fs",
+        "version": 2,
+        "uniforms": {},
+    }
+}
