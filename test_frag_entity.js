dee19bb374476e96ecb4a0878dbe5b6f41276c49
Polyrhythm <ryan.joshua.jones@gmail.com>
2016-11-26 09:54:09 -0800

    Update test_frag_entity.js

--- test_frag_entity.js
+++ test_frag_entity.js
@@ -1,9 +1,10 @@
 // https://raw.githubusercontent.com/Polyrhythm/hifi-scripts/master/test_frag.fs
 (function() {
-    this.userData = {
-    ProceduralEntity: {
-        shaderUrl: "https://raw.githubusercontent.com/Polyrhythm/hifi-scripts/master/test_frag.fs",
-        version: 2,
-        uniforms: {},
-    }    
+    this.userData = JSON.stringify({
+        ProceduralEntity: {
+            shaderUrl: "https://raw.githubusercontent.com/Polyrhythm/hifi-scripts/master/test_frag.fs",
+            version: 2,
+            uniforms: {},
+        }
+    })
 })
