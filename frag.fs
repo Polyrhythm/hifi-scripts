209e65105a7172009fcfe415e9dcc651a3fa0f51
Polyrhythm <ryan.joshua.jones@gmail.com>
2016-11-26 10:53:01 -0800

    Update frag.fs

--- frag.fs
+++ frag.fs
@@ -1,18 +1,12 @@
-// Produce a lit procedural surface
-float getProceduralColorsLit(inout vec3 diffuse, inout vec3 specular, inout float shininess) {
-    vec3 noiseColor = getNoiseColor();
-    diffuse = noiseColor;
-    return 0.0;
-}
-
-// Produce an unlit procedural surface:  emulates old behavior
-float getProceduralColorsUnlit(inout vec3 diffuse, inout vec3 specular, inout float shininess) {
-    vec3 noiseColor = getNoiseColor();
-    diffuse = vec3(1.0);
-    specular = noiseColor;
-    return 1.0;
-}
-
 float getProceduralColors(inout vec3 diffuse, inout vec3 specular, inout float shininess) {
-    return getProceduralColorsLit(diffuse, specular, shininess);
+    vec2 uv = _position.xz / iResolution.xy;
+    vec2 p = -1.0 + 2.0 * uv;
+
+    vec3 colour = vec3(1.0);
+
+    float map = step(0.1, distance(p, vec2(0.0)));
+
+    diffuse = vec3(map, 1.0, 1.0);
+    
+    return 0.0;
 }
