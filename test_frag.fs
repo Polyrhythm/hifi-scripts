d31da83db733f1cbf589cf75cd82da1881229be6
Polyrhythm <ryan.joshua.jones@gmail.com>
2016-11-26 09:06:24 -0800

    Update test_frag.fs

--- test_frag.fs
+++ test_frag.fs
@@ -1,6 +1,6 @@
 precision highp float;
 
-void main(void) {
+void main(out vec4 gl_FragColor, in vec2 gl_FragCoord) {
     vec2 uv = gl_FragCoord.xy / iResolution.xy;
     vec2 p = -1.0 + 2.0 * uv;
 
@@ -8,5 +8,5 @@ void main(void) {
 
     float map = step(0.1, distance(p, vec2(0.0)));
 
-    outColour = vec4(vec3(map, 1.0, 1.0), 1.0);
+    gl_FragColor = vec4(vec3(map, 1.0, 1.0), 1.0);
 }
